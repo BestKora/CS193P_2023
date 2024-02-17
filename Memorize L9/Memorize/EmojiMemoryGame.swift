245337b3695275adcceabaac1fc9572139ffccb6
BestKora <developer@bestkora.com>
2024-02-17 14:37:27 +0300

    L9

---
+++ Memorize L9/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,47 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by CS193P instructor on 4/03/23.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    typealias  Card = MemoryGame<String>.Card
+    private static let emojis =
+        ["👻","🎃","🕷️","😈","💀","🕸️","🧙‍♀️","🙀","👹","😱","☠️","🍭"]
+    
+    private static func createMemoryGame() -> MemoryGame<String>{
+        return MemoryGame( numberOfPairsOfCards: 7) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<Card> {
+       model.cards
+    }
+    
+    var color: Color {
+        .orange
+    }
+    
+    var score: Int {
+        model.score
+    }
+    
+    // MARK: - Intents
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose (_ card: Card) {
+        return model.choose(card)
+    }
+}
